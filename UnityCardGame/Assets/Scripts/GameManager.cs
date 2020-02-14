6047c688a9e48a00c6b8b8198dcd1a323ccd67c0
Timeteddy <59497727+Timeteddy@users.noreply.github.com>
2020-02-14 08:05:14 +0800

    卡片遊戲玩成

--- UnityCardGame/Assets/Scripts/GameManager.cs
+++ UnityCardGame/Assets/Scripts/GameManager.cs
@@ -1,39 +1,65 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     #region KID
     [Header("卡片陣列")]
-    public GameObject[] cards;
+    public MeshFilter[] cards;
     [Header("發牌按鈕")]
     public Button btnGetCard;
+
+    public Animator ani_deck;
+    public MeshFilter m_pc, m_player;
     
     private int player, pc;     // 玩家、電腦卡片編號
+
+    //發牌
+    public GameObject settlement;
+    public Text text_settlement;
+
+    //換牌
+    public GameObject changView;
 
     private void Start()
     {
         aud = GetComponent<AudioSource>();
+
+        player = shuffle();
+        pc = shuffle();
     }
 
     /// <summary>
-    /// 玩家取得卡片
+    /// 洗牌
+    /// </summary>
+    /// <returns></returns>
+    int shuffle()
+    {
+        int r = Random.Range(0, cards.Length);
+
+        return r;
+    }
+
+    /// <summary>
+    /// 遊戲開始
     /// </summary>
     public void PlayerGetCard()
     {
         btnGetCard.interactable = false;
-        player = GetCard(new Vector3(0, -3, 0));
+        changeCards();
+        ani_deck.SetTrigger("licensing");
 
-        Invoke("PcGetCard", 1.5f);
-        Invoke("GameWinner", 2.5f);
+        Invoke("openChangeView", 2.0f);
     }
 
     /// <summary>
-    /// 電腦取得卡片
+    /// 更換卡片顯示
     /// </summary>
-    private void PcGetCard()
+    private void changeCards()
     {
-        pc = GetCard(new Vector3(0, 3, 0));
+        m_player.sharedMesh = cards[player].sharedMesh;
+        m_pc.sharedMesh = cards[pc].sharedMesh;
     }
 
     /// <summary>
@@ -41,16 +67,16 @@ public class GameManager : MonoBehaviour
     /// </summary>
     /// <param name="pos">卡片座標</param>
     /// <returns>取得的卡片編號</returns>
-    private int GetCard(Vector3 pos)
-    {
-        aud.PlayOneShot(soundGetCard);
+    //private int GetCard(Vector3 pos)
+    //{
+    //    aud.PlayOneShot(soundGetCard);
 
-        int r = Random.Range(0, cards.Length);
+    //    int r = Random.Range(0, cards.Length);
 
-        Instantiate(cards[r], pos, Quaternion.Euler(0, 180, 0));
+    //    Instantiate(cards[r], pos, Quaternion.Euler(0, 180, 0));
 
-        return r + 1;
-    }
+    //    return r + 1;
+    //}
     #endregion
 
     #region 練習區域
@@ -71,7 +97,75 @@ public class GameManager : MonoBehaviour
     /// </summary>
     private void GameWinner()
     {
-        
+        settlement.SetActive(true);
+
+        if (player > pc)
+        {
+            text_settlement.text = "獲勝";
+            aud.PlayOneShot(soundWin);
+        }
+        else if(player < pc)
+        {
+            text_settlement.text = "輸了";
+            aud.PlayOneShot(soundLose);
+        }
+        else
+        {
+            text_settlement.text = "平手";
+            aud.PlayOneShot(soundTie);
+        }
     }
+
+    /// <summary>
+    /// 開啟更換的畫面
+    /// </summary>
+    public void openChangeView()
+    {
+        changView.SetActive(true);
+    }
+
+    /// <summary>
+    /// 重新開始
+    /// </summary>
+    public void again()
+    {
+        SceneManager.LoadScene("練習場景");
+    }
+
+    /// <summary>
+    /// 實際進行換牌
+    /// </summary>
+    public void changeValue()
+    {
+        int proxy;
+
+        proxy = player;
+        player = pc;
+        pc = proxy;
+    }
+
+    /// <summary>
+    /// 換牌
+    /// </summary>
+    public void yesChange()
+    {
+        changView.SetActive(false);
+        changeValue();
+        changeCards();
+        ani_deck.SetTrigger("showdown");
+        Invoke("GameWinner", 2);
+    }
+
+    /// <summary>
+    /// 不換牌
+    /// </summary>
+    public void noChange()
+    {
+        changView.SetActive(false);
+        ani_deck.SetTrigger("showdown");
+        Invoke("GameWinner", 2);
+    }
+
+    
     #endregion
 }
---
+++ UnityCardGame/Assets/Scripts/PlayAudio.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class PlayAudio : MonoBehaviour
+{
+    public AudioSource aud;        // 音效來源：喇叭
+    public AudioClip soundGetCard;  // 發牌
+
+    public void event_Deck()
+    {
+        aud.PlayOneShot(soundGetCard);
+    }
+}
